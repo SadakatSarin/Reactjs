1b3cdc949e06bbd06d8bf80a9f6862e91e70c4f1
unknown <SadakatSarin>
2025-05-04 09:22:08 +0600

    Counter with callback

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState,useCallback} from 'react';
 
 const Counter = () => {
 
-    const [counter,setCounter] = useState(1);
+    const [counter, setCounter] = useState(0);
+    const handleCounter = useCallback((value) => {
+
+        setCounter((prev) => prev + value);
+
+    });
+
     return (
         <div>
             <h1>counter:{counter}</h1>
+            <button onClick={()=>handleCounter(-1)}>-</button> 
+            <button  onClick={()=>handleCounter(1)}>+</button>
         </div>
     );
 };
